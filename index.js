267531d11ac8f94f0c9ddebeff20748638b6e73a
nona12122 <qosayhaneen90@gmail.com>
2026-07-23 19:05:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import 'dotenv/config';
 import {Client,GatewayIntentBits} from 'discord.js';
 const client=new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
 
-const replies={
-  "ليبا":"https://i.imgur.com/h4YiTP8.gif",
-  "قوانين":"https://example.com/rules.png"
+const replies = {
+  "نونا": "<:C1_:1510998417428582472>     أثري في الغياب يفوق حضور الكثيرين     <:5f319c37b0704b398aef627fbdfee90f:1471948383747834070>\n\nhttps://i.imgur.com/h4YiTP8.gif",
+  "قوانين": "https://example.com/rules.png"
 };
 
 client.on("messageCreate",msg=>{
