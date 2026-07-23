6d0cff6ff7c6f26714bc7939925055953763061b
nona12122 <qosayhaneen90@gmail.com>
2026-07-23 19:15:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {Client,GatewayIntentBits} from 'discord.js';
 const client=new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
 
 const replies = {
-  "نونا": "<:C1_:1510998417428582472>     أثري في الغياب يفوق حضور الكثيرين     <:5f319c37b0704b398aef627fbdfee90f:1471948383747834070>\n\nhttps://i.imgur.com/h4YiTP8.gif",
+  "نونا": "أثري في الغياب يفوق حضور الكثيرين.\n\nhttps://i.imgur.com/h4YiTP8.gif",
   "قوانين": "https://example.com/rules.png"
 };
 
