d12a5510cfce0c946c2ff4338cf5c251c85e3aea
nona12122 <qosayhaneen90@gmail.com>
2026-07-24 15:22:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,11 @@ client.on("messageCreate", async (msg) => {
     await msg.channel.send("https://i.imgur.com/h4YiTP8.gif");
   }
 
-  if (t === "قوانين" && msg.channel.id === "1356618087448838186") {
+ if (
+  t === "قوانين" &&
+  msg.channel.id === "1356618087448838186" &&
+  msg.member.roles.cache.has("1530170505070645369")
+) {
 
     const embed = new EmbedBuilder()
       .setColor("#b388ff")
@@ -85,5 +89,59 @@ client.on("guildMemberUpdate", async (oldMember, newMember) => {
     });
   }
 });
+// ==========================
+// Anti Spam Protection
+// ==========================
 
+const spamMap = new Map();
+
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot || !msg.guild) return;
+
+  // استثناء الإدارة
+  if (msg.member.roles.cache.has("1530170505070645369")) return;
+
+  // استثناء رتبة Protection
+  if (msg.member.roles.cache.has("1530179158494150779")) return;
+
+  const userId = msg.author.id;
+  const now = Date.now();
+
+  if (!spamMap.has(userId)) {
+    spamMap.set(userId, []);
+  }
+
+  const timestamps = spamMap.get(userId);
+
+  // حذف الرسائل الأقدم من 5 ثواني
+  while (timestamps.length && now - timestamps[0] > 5000) {
+    timestamps.shift();
+  }
+
+  timestamps.push(now);
+
+  // 8 رسائل خلال 5 ثواني
+  if (timestamps.length >= 8) {
+
+    spamMap.delete(userId);
+
+    try {
+      await msg.member.timeout(
+        10 * 60 * 1000,
+        "Spam Protection"
+      );
+
+      const logChannel = msg.guild.channels.cache.get("1356618087448838186");
+
+      if (logChannel) {
+        await logChannel.send(
+          `🚨 | تم إعطاء ${msg.member} Timeout لمدة **10 دقائق** بسبب السبام.`
+        );
+      }
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
 client.login(process.env.TOKEN);
