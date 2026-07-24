98d97b98b7eaffbc6271cd1d79642f3c4d849f01
nona12122 <qosayhaneen90@gmail.com>
2026-07-24 13:13:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { Client, GatewayIntentBits } from 'discord.js';
+import { joinVoiceChannel } from '@discordjs/voice';
 
 const client = new Client({
   intents: [
@@ -7,6 +8,26 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
   ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  const guild = client.guilds.cache.get("1356618086635016292");
+  if (!guild) return;
+
+  const channel = guild.channels.cache.get("1372931441821880382");
+  if (!channel) return;
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+    selfMute: true
+  });
+
+  console.log("Joined voice channel!");
 });
 
 client.on("messageCreate", async (msg) => {
